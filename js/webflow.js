7f3e4cd45ed1e1b7e73adcb037be408c295bb031
Robert A. Bunton <robertabunton@icloud.com>
2023-11-30 13:15:38 -0800

    DOMLoader

--- js/webflow.js
+++ js/webflow.js
@@ -1,14 +1,13 @@
 
-// Wait for the entire window to load, including scripts and Lottie animations
-window.onload = function() {
-    // Selecting the elements
+document.addEventListener('DOMContentLoaded', function() {
     var cardsButton = document.getElementById('cards-button');
     var spacerBottom = document.querySelector('.spacer.bottom');
     var secretFooter = document.getElementById('secret');
 
-    // Adding click event listener to the cards button
-    cardsButton.addEventListener('click', function() {
-        spacerBottom.classList.toggle('hidden');
-        secretFooter.classList.toggle('visible');
-    });
-};
+    if (cardsButton) {
+        cardsButton.addEventListener('click', function() {
+            spacerBottom.classList.toggle('hidden');
+            secretFooter.classList.toggle('visible');
+        });
+    }
+});
